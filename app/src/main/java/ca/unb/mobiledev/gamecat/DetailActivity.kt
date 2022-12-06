f14c2f92fa218345379c766f10d70b2db9547f5e
bmacleo2UNB3035 <bmacleo2@unb.ca>
2022-12-05 21:01:35 -0400

    Added delete feature. Works by long pressing the game in the main activity.

--- app/src/main/java/ca/unb/mobiledev/gamecat/DetailActivity.kt
+++ app/src/main/java/ca/unb/mobiledev/gamecat/DetailActivity.kt
@@ -20,7 +20,7 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-
+        val viewModel = ViewModelProvider(this)[GameViewModel::class.java]
 
         // Get intents
         val title = intent.getStringExtra("title")
@@ -67,10 +67,15 @@ class DetailActivity : AppCompatActivity() {
         //   This might help you - http://developer.android.com/reference/android/support/v7/app/AppCompatActivity.html#getSupportActionBar%28%29
         // TODO Delete function
 
-        val deleteButton = findViewById<Button>(R.id.deleteButton)
+        /*val deleteButton = findViewById<Button>(R.id.deleteButton)
+
         deleteButton.setOnClickListener {
 
-        }
+                   val game: Game? = viewModel.allGames.value?.get(1)
+            if (game != null) {
+                viewModel.delete(game)
+            }
+        }*/
     }
 
     //Taken from here https://github.com/prasadankitt/ImageApp/blob/main/app/src/main/java/com/example/cameragallery/Converter.kt
